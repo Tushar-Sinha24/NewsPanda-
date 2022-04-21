65c9f5d5a4b11f78cc1870d1708d9444c84067fe
Tushar <sinhatushar9234@gmail.com>
2022-04-21 13:14:18 +0530

    commit

--- src/component/News.js
+++ src/component/News.js
@@ -7,45 +7,62 @@ export class News extends Component {
       super();
       this.state={
         articles:[],
-        
+        loading:false,
+        page:1
+
       }
     }
+    
     async componentDidMount(){
-      let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=0f422d94684048759b3ae0f2ed13f3fb&page=2";
+      let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=0f422d94684048759b3ae0f2ed13f3fb&page=1&pageSize=20";
      let data =await fetch(url);
      let parsedData= await data.json()
     //  console.log(parsedData);
-     this.setState({articles:parsedData.articles})
+     this.setState({articles:parsedData.articles, totalResults:parsedData.totalResults})
     }
-
-    prev= ()=>{
-      console.log("ASDsa")
-    }
-     next= async()=>{
-      let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=0f422d94684048759b3ae0f2ed13f3fb&page=${this.state.page + 1}`;
+    
+    prev= async()=>{
+      let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=0f422d94684048759b3ae0f2ed13f3fb&page=${this.state.page-1}&pageSize=20`;
       let data =await fetch(url);
       let parsedData= await data.json()
-      
+     //  console.log(parsedData);
+     
+       this.setState({
+         page:this.state.page-1,
+         articles:parsedData.articles
+       })
+    }
+     next= async()=>{
+
+
+      if(this.state.page+1>Math.ceil(this.state.totalResults/20)){}
+      else{
+      let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=0f422d94684048759b3ae0f2ed13f3fb&page=${this.state.page+1}&pageSize=20 `;
+     let data =await fetch(url);
+     let parsedData= await data.json()
+    //  console.log(parsedData);
+    
       this.setState({
-        page:this.state.page + 1,
+        page:this.state.page+1,
         articles:parsedData.articles
-      })
+      })}
     }
   render() {
     return (
+      
       <div className='container-fluid'>
         <div className="container my-3">
             <h1>NewsPanda-Top Headlines</h1>
             <div className="row">
               {this.state.articles.map((element)=>{
-                 return <div className="col-md-4" key={element.urlToImage}>
+                 return <div className="col-md-4" key={element.url}>
                  <NewsItem  title={element.title} description={element.description}  imgUrl={element.urlToImage} newsUrl={element.url}/>
                  </div>
               })}     
             </div>
             <div className="d-flex justify-content-between">
-            <button type="button" disabled={this.state.page<=1?false:true} onClick={this.prev} className="btn btn-dark"> &larr; prev</button>
-            <button type="button" onClick={this.next} className="btn btn-dark">Next &rarr;</button>
+            <button type="button" disabled={this.state.page<=1} onClick={this.prev} className="btn btn-dark"> &larr; prev  </button>
+            <button type="button" disabled={this.state.page+1>Math.ceil(this.state.totalResults/20)} onClick={this.next} className="btn btn-dark">Next &rarr;</button>
             </div>
             
         </div>
